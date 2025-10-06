1dcbb946b3818ca988b7afa91dc95d680f9045a8
ifahimkhan <khan.fahim95@hotmail.com>
2025-10-06 11:00:49 +0530

    feat: Implement user data insertion on button click
    Adds functionality to insert a new `UserData` object into the Room database when the "submit" button is clicked.
    
    -   Initializes the `UserDatabase` instance.
    -   Creates a single-thread `ExecutorService` to run database operations on a background thread.
    -   Adds an `OnClickListener` to the "submit" `Button`.
    -   On click, a hardcoded `UserData` object is created and inserted into the database using the DAO.

--- app/src/main/java/com/fahim/m2practice/MainActivity.java
+++ app/src/main/java/com/fahim/m2practice/MainActivity.java
@@ -1,6 +1,7 @@
 package com.fahim.m2practice;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +9,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class MainActivity extends AppCompatActivity {
+
+    UserDatabase database;
+    ExecutorService executorService = Executors.newSingleThreadExecutor();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        database = UserDatabase.getInstance(this);
+        Button submit = findViewById(R.id.submit);
+        submit.setOnClickListener(view -> {
+            UserData userObject = new UserData();
+            userObject.Username = "Fahim";
+            userObject.dateOfbirth = "12/12/2000";
+            executorService.execute(() -> {
+                database.userDao().insert(userObject);
+            });
+        });
+
     }
 }
